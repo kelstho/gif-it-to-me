5c6b8710cb1deedda477bb3a98ca505723592bd2
markspcs <markspcs@yahoo.com>
2020-03-23 20:04:23 -0500

    add dynamic boardsv2

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -26,7 +26,7 @@ module.exports = function(app) {
   app.get("/members", isAuthenticated, function(req, res) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
-  app.get("/gameBoard", function(req, res){
+  app.get("/gameBoard", function(req, res) {
     app.gameName = req.query.gameName;
     res.sendFile(path.join(__dirname, "../public/gameboard.html"));
     // res.json({
