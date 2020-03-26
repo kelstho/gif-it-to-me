62b89f2c436e03f9c291a101265176e563005d57
INWYTIA <inwytia@gmail.com>
2020-03-25 21:03:31 -0500

    beutification

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -1,19 +1,24 @@
 $(document).ready(function() {
-  
-  $("#start-button").on("click", function () {
-    var myBoard = $("#make-game-input").val().trim();
-    var players = $("#how-many-players").val().trim();
+  $("#start-button").on("click", function() {
+    var myBoard = $("#make-game-input")
+      .val()
+      .trim();
+    var players = $("#how-many-players")
+    .val()
+    .trim();
     var boardData = {
-      boardName : myBoard,
-      playerNum : players
+      boardName: myBoard,
+      playerNum: players
     };
     $.post("/creategame", boardData);
   });
 
-  $("#join-button").on("click", function () {
-    var gameID = $("#join-game-input").val().trim();
+  $("#join-button").on("click", function() {
+    var gameID = $("#join-game-input")
+      .val()
+      .trim();
     var gameData = {
-      boardName : gameID
+      boardName: gameID
     };
     $.post("/join", gameData);
   });
