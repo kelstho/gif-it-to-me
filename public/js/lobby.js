16a1a217d453ac8a968751926408ebdb11776ace
INWYTIA <inwytia@gmail.com>
2020-03-25 20:31:49 -0500

    index.js now routes and is renamed to lobby.js, updated index.html to match, added maxPlayer column to game.js, moved socket to gameboard.js

---
+++ public/js/lobby.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  
+  $("#start-button").on("click", ()=>{
+    let newBoard = $("#make-game-input").val().trim();
+    let players = $("#how-many-players").val().trim();
+    let boardData = {
+      boardName : newBoard,
+      playerNum : players
+    };
+    $.post("/creategame", boardData);
+  });
+
+  $("#join-button").on("click", () => {
+    let gameID = $("#join-game-input").val().trim();
+    let gameData = {
+      boardName : gameID
+    };
+    $.post("/join", boardData);
+  });
+});
